72133b90822a87e909e38c9ea22f9a64496a8364
hcorpaf <hc96.corpa5@gmail.com>
2022-07-31 07:17:05 +0200

    arreglao

--- Assets/Scripts/EndingsManager.cs
+++ Assets/Scripts/EndingsManager.cs
@@ -1,37 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class EndingsManager : MonoBehaviour
-{
-    int maxLevelValue;
-    int moneyLevel;
-    int credibilityLevel;
-    int pressureLevel;
+// public class EndingsManager : MonoBehaviour
+// {
+//     int maxLevelValue;
+//     int moneyLevel;
+//     int credibilityLevel;
+//     int pressureLevel;
 
-    // Gestionar finales en funcion de los valores de los niveles de las métricas
-    public void CriticalEndings(){
-        if (moneyLevel <= 0){
-            // Final por pérdida de dinero: quiebra del periódico
-            if (allTrue) AllwaysTrueEnding(); // Despido con conciencia tranquila
-            else LowMoneyEnding();
-        } else if (credibilityLevel <= 0){
-            // Final por pérdida de credibilidad: despido del periódico
-            LowCredibilityEnding();
-        } else if (pressureLevel >= maxLevelValue){
-            // Final por ganancia de presión: Muerte misteriosa en un accidente y reemplazo en el periódico
-            HighPressureEnding();
-        }
-    }
+//     // Gestionar finales en funcion de los valores de los niveles de las métricas
+//     public void CriticalEndings(){
+//         if (moneyLevel <= 0){
+//             // Final por pérdida de dinero: quiebra del periódico
+//             if (allTrue) AllwaysTrueEnding(); // Despido con conciencia tranquila
+//             else LowMoneyEnding();
+//         } else if (credibilityLevel <= 0){
+//             // Final por pérdida de credibilidad: despido del periódico
+//             LowCredibilityEnding();
+//         } else if (pressureLevel >= maxLevelValue){
+//             // Final por ganancia de presión: Muerte misteriosa en un accidente y reemplazo en el periódico
+//             HighPressureEnding();
+//         }
+//     }
 
-    // Gestionar finales en funcion de los valores de los niveles de las métricas
-    public void AlternativeEndings(){
-        if (elisaStrikes >= 2){
-            // Final Elisa es despedida: 2 noticias que perjudiquen a Elisa
-            ElisaFiredEnding();
-        } else if (lastLevelCompleted){
-            // Final completando todos los niveles: vida mediocre en el periódico
-            MediocreEnding();
-        }
-    }
-}
+//     // Gestionar finales en funcion de los valores de los niveles de las métricas
+//     public void AlternativeEndings(){
+//         if (elisaStrikes >= 2){
+//             // Final Elisa es despedida: 2 noticias que perjudiquen a Elisa
+//             ElisaFiredEnding();
+//         } else if (lastLevelCompleted){
+//             // Final completando todos los niveles: vida mediocre en el periódico
+//             MediocreEnding();
+//         }
+//     }
+// }
